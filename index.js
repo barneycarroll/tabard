17fcef47dd33501bdcc05685380d066528831d45
Barney Carroll <barney.carroll@gmail.com>
2015-08-25 14:36:41 +0100

    Some code

---
+++ index.js
@@ -0,0 +1,23 @@
+const { filter, sort } = Array.prototype
+const style            = getComputedStyle || ( e => e.currentStyle )
+const inputs           = /input|select|textarea|button|object/i
+const links            = /a|area/i
+
+export default () =>
+	document.getElementsByTagName( '*' )
+		// DOM conditions
+		::filter( e => 
+				e.tabIndex != -1 && (
+					inputs.test( e.tagName ) && !e.disabled || 
+					links.test( e.tagName ) && ( e.href || e.tabIndex )
+				)
+		)
+		// CSSOM conditions
+		::filter( e =>
+			style( e ).visibility != 'hidden' &&
+			style( e ).display    != 'none'
+		)
+		// Sort by tabIndex
+		::sort( ( a, b ) => 
+			a.tabIndex == b.tabIndex ? 0 : a.tabIndex > b.tabIndex ? 1 : -1
+		)
