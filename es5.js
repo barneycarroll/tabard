c2a9c7e0a021b0658dda480bd10733290bda4c81
Barney Carroll <barney.carroll@gmail.com>
2016-09-30 11:38:24 +0100

    Punctuation is important

--- es5.js
+++ es5.js
@@ -8,19 +8,19 @@ new function(){
       document.getElementsByTagName( '*' )
     )
       // DOM conditions
-      filter( function( e ){ return(
+      .filter( function( e ){ return(
           e.tabIndex != -1 && (
             inputs.test( e.tagName ) && !e.disabled ||
             links.test( e.tagName ) && ( e.href || e.tabIndex )
           )
       ) } )
       // CSSOM conditions
-      filter( function( e ){ return(
+      .filter( function( e ){ return(
         style( e ).visibility != 'hidden' &&
         style( e ).display    != 'none'
       ) } )
       // Sort by tabIndex
-      sort( function( a, b ){ return(
+      .sort( function( a, b ){ return(
         a.tabIndex == b.tabIndex ? 0 : a.tabIndex > b.tabIndex ? 1 : -1
       ) } )
   }
--- es6.js
+++ es6.js
@@ -5,18 +5,18 @@ const links            = /a|area/i
 export default () =>
   Array.from( document.getElementsByTagName( '*' ) )
     // DOM conditions
-    filter( e =>
+    .filter( e =>
         e.tabIndex != -1 && (
           inputs.test( e.tagName ) && !e.disabled ||
           links.test( e.tagName ) && ( e.href || e.tabIndex )
         )
     )
     // CSSOM conditions
-    filter( e =>
+    .filter( e =>
       style( e ).visibility != 'hidden' &&
       style( e ).display    != 'none'
     )
     // Sort by tabIndex
-    sort( ( a, b ) =>
+    .sort( ( a, b ) =>
       a.tabIndex == b.tabIndex ? 0 : a.tabIndex > b.tabIndex ? 1 : -1
     )
